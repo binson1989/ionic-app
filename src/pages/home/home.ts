e7c793aedee77b6a6dffe5d2c5ec6f2934acc257
binson <binson1989@gmail.com>
2017-10-02 21:58:54 +0530

    add detail page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailPage } from "../detail/detail";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: any[];
 
   constructor(public navCtrl: NavController) {
-
+    this.items = [];
+    for (var index = 0; index < 100; index++) {
+      this.items.push({
+        text: 'Item ' + index,
+        id: index
+      });
+    }
   }
 
+  itemSelected(item): void {
+    this.navCtrl.push(DetailPage, { item: item });
+  }
 }
